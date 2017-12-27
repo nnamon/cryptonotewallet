0b12b5131e374fbabd1a0169cd3e343b546d3f73
amon <amon@nandynarwhals.org>
2017-12-27 21:18:47 +0800

    QUANYANGCOIN

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "quanyangcoin")
+set(CN_CURRENCY_DISPLAY_NAME "QuanyangCoin")
+set(CN_CURRENCY_TICKER "QYC")
